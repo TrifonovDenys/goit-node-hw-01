41ced90ac241f27956bd24fabc8cc22597cac503
Denys Tryfonov <trifonowden123@gmail.com>
2023-08-29 11:44:42 +0300

    gitignore

--- contacts.js
+++ contacts.js
@@ -1,56 +1,62 @@
-
-
 const fs = require("fs").promises;
-// const fs = require('fs');
-fs.readFile("contacts.js").then((data) => console.log(data.toString()));
 
-/*
- * Раскомментируй и запиши значение */
-const contactsPath = './db/contacts.json';
-
+/* Раскомментируй и запиши значение */
+const contactsPath = "./db/contacts.json";
 
 // TODO: задокументировать каждую функцию
-function listContacts() {
+async function listContacts() {
   // ...твой код. Возвращает массив контактов.
-  fs.readFile(contactsPath)
-    .then(data => console.log(data.toString()))
-    .catch(err => console.log(err.message))
+  const list = await fs
+    .readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((res) => console.table(res))
+    .catch((err) => console.log(err.message));
+  return list;
 }
+
+// async function listContacts() {
+//   try {
+//     const data = await fs.readFile(contactsPath, "utf8"); // чтение файла асинхронно
+//     const list = JSON.parse(data); // преобразование данных из JSON
+//     return list;
+//   } catch (err) {
+//     console.error(err.message);
+//     throw err; // выбросить ошибку, чтобы вызывающий код мог обработать ее
+//   }
+// }
 
 async function getContactById(contactId) {
   // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
-
   try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data)
-    const contact = contacts.find(contact => contact.id === contactId)
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
     if (contact) {
       console.log(contact);
-      return contact
+      return contact;
     } else {
       console.log(null);
-      return null
+      return null;
     }
-  } catch(err) {
-    console.log(err.message)
+  } catch (err) {
+    console.log(err.message);
   }
 }
 
-
-
 async function removeContact(contactId) {
   // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
-
   try {
-    const data = await fs.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data)
-    const contactIndex = contacts.findIndex(contact => contact.id === contactId)
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
     if (contactIndex === -1) {
       console.log(null);
-      return null
-    }
-    else {
+      return null;
+    } else {
       console.log(contacts.splice(contactIndex, 1));
+      // return contacts.splice(contactIndex, 1);
       // return await fs.writeFile(contactsPath, JSON.stringify(contacts))
     }
   } catch (err) {
@@ -58,24 +64,21 @@ async function removeContact(contactId) {
   }
 }
 
-
 async function addContact(name, email, phone) {
-  // ...твой код. Возвращает объект добавленного контакта. 
-  
-  
+  // ...твой код. Возвращает объект добавленного контакта.
   try {
     // const data = await fs.readFile(contactsPath, 'utf-8');
     // const contacts = JSON.parse(data)
     // contacts.push({ name: name, email: email, phone: phone })
     // console.log(contacts);
-    return { name: name, email: email, phone: phone }
+    console.log("addConact res = ", { name: name, email: email, phone: phone });
+    return { name: name, email: email, phone: phone };
   } catch (err) {
     console.log(err.message);
   }
 }
 
-
-addContact("Allen Raymond", "dui.in@egetlacus.ca", "(501) 472-5218")
+// addContact("Allen Raymond", "dui.in@egetlacus.ca", "(501) 472-5218")
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log(1);
+const func = require("./contacts");
+// const argv = require("yargs").argv;
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+// console.log("list", func.listContacts());
+
+// console.log("get", func.getContactById("AeHIrLTr6JkxGE6SN-0Rw"));
+
+// console.log("remove", func.removeContact("AeHIrLTr6JkxGE6SN-0Rw"));
+
+// console.log(
+//   "add",
+//   func.addContact("Allen Raymond", "dui.in@egetlacus.ca", "(501) 472-5218")
+// );
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      func.listContacts()
+      break;
+
+    case "get":
+      func.getContactById(id);
+      break;
+
+    case "add":
+      func.addContact(name, email, phone);
+      break;
+
+    case "remove":
+      func.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
